3af834b7c6bb5eea65f41e31fd99abdffddfe2cb
William Su <williamsu@Williams-MacBook-Pro.local>
2024-05-09 16:13:41 +0800

    added secure headers as middleware

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -68,14 +68,3 @@ func openDB(dsn string) (*sql.DB, error) {
 	}
 	return db, err
 }
-
-func (app *application) routes() *http.ServeMux {
-	mux := http.NewServeMux()
-	mux.HandleFunc("/", app.home)
-	mux.HandleFunc("/snippet", app.showSnippet)
-	mux.HandleFunc("/snippet/create", app.createSnippet)
-
-	fileServer := http.FileServer(http.Dir("./ui/static"))
-	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
-	return mux
-}
