a3ff787b76aa608e0e672fdea761e58549d0a52f
William Su <williamsu@Williams-MacBook-Pro.local>
2024-05-09 12:55:34 +0800

    added html template cache

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -14,32 +14,16 @@ func (app *application) home(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	s, err := app.snippets.Latest()
+	snippets, err := app.snippets.Latest()
 	if err != nil {
 		app.serverError(w, err)
 		return
 	}
 
-	for _, snippet := range s {
-		fmt.Fprintf(w, "%v\n", snippet)
-	}
+	app.render(w, r, "home.page.tmpl", &templateData{
+		Snippets: snippets,
+	})
 
-	// files := []string{
-	// 	"./ui/html/home.page.tmpl",
-	// 	"./ui/html/base.layout.tmpl",
-	// 	"./ui/html/footer.partial.tmpl",
-	// }
-	// ts, err := template.ParseFiles(files...)
-	// if err != nil {
-	// 	app.serverError(w, err)
-
-	// }
-	// err = ts.Execute(w, nil)
-	// if err != nil {
-	// 	app.serverError(w, err)
-	// }
-
-	// w.Write([]byte("Hello from Snippet!"))
 }
 
 func (app *application) showSnippet(w http.ResponseWriter, r *http.Request) {
@@ -58,7 +42,9 @@ func (app *application) showSnippet(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	fmt.Fprintf(w, "%v", snippet)
+	app.render(w, r, "show.page.tmpl", &templateData{
+		Snippet: snippet,
+	})
 }
 
 func (app *application) createSnippet(w http.ResponseWriter, r *http.Request) {
--- cmd/web/helpers.go
+++ cmd/web/helpers.go
@@ -19,3 +19,17 @@ func (app *application) clientError(w http.ResponseWriter, status int) {
 func (app *application) notFound(w http.ResponseWriter) {
 	app.clientError(w, http.StatusNotFound)
 }
+
+func (app *application) render(w http.ResponseWriter, r *http.Request, name string, tempData *templateData) {
+	temp, ok := app.templateCache[name]
+	if !ok {
+		app.serverError(w, fmt.Errorf("template %s does not exist", name))
+		return
+	}
+
+	err := temp.Execute(w, &tempData)
+	if err != nil {
+		app.serverError(w, err)
+		return
+	}
+}
