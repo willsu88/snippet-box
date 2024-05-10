ea1d1d4d3bd70f6a6ab5e6e841056976d69e4c85
William Su <williamsu@Williams-MacBook-Pro.local>
2024-05-10 14:52:28 +0800

    adding process form

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -12,9 +12,9 @@ func (app *application) routes() http.Handler {
 
 	mux := pat.New()
 	mux.Get("/", http.HandlerFunc(app.home))
-	mux.Get("/snippet/:id", http.HandlerFunc(app.showSnippet))
 	mux.Post("/snippet/create", http.HandlerFunc(app.createSnippet))
 	mux.Get("/snippet/create", http.HandlerFunc(app.createSnippetForm))
+	mux.Get("/snippet/:id", http.HandlerFunc(app.showSnippet))
 
 	fileServer := http.FileServer(http.Dir("./ui/static"))
 	mux.Get("/static/", http.StripPrefix("/static", fileServer))
