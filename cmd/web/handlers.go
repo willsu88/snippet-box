d12898e7ddc841dc7570f31e2a446ebb4f20d0e4
William Su <williamsu@Williams-MacBook-Pro.local>
2024-05-16 16:30:01 +0800

    adding login and hash password

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -102,6 +102,20 @@ func (app *application) signupUser(w http.ResponseWriter, r *http.Request) {
 		app.errorLog.Printf("%s - %s %s", r.RemoteAddr, r.Proto, r.Method)
 		return
 	}
+
+	err = app.users.Insert(form.Get("name"), form.Get("email"), form.Get("password"))
+	if err == models.ErrDuplicateEmail {
+		form.Errors.Add("email", "address is already in use")
+		app.render(w, r, "signup.page.tmpl", &templateData{Form: form})
+		return
+	} else if err != nil {
+		app.serverError(w, err)
+		return
+	}
+
+	app.session.Put(r, "flash", "Signup successful. Please log in.")
+	http.Redirect(w, r, "/user/login", http.StatusSeeOther)
+
 }
 
 func (app *application) loginUserForm(w http.ResponseWriter, r *http.Request) {
